fe093ae370adb826abc8b926e0d64428de9a1efb
unknown <ajoshi@EMBIOPTERA.peerius.local>
2015-02-20 15:41:34 +0000

    "Changes"

--- SmartManager/src/main/java/com/peerius/utils/Context.java
+++ SmartManager/src/main/java/com/peerius/utils/Context.java
@@ -220,7 +220,7 @@ public class Context extends COREManager {
 		driverInstance.switchTo().alert().dismiss();
 	}
 	
-	public void verifyInnerHTML(By elementlocator, String text) {
+	public static void verifyInnerHTML(By elementlocator, String text) {
 		
 		WebElement innerHTML = driverInstance.findElement(elementlocator);
 		
