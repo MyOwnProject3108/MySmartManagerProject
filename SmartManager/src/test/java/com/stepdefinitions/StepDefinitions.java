8a7fac6493df1ac9be1239826b0556b5b99e1514
Zach <Abba@192.168.1.99>
2015-03-01 04:12:13 +0000

    Changes Steps Def

--- SmartManager/src/test/java/com/stepdefinitions/StepDefinitions.java
+++ SmartManager/src/test/java/com/stepdefinitions/StepDefinitions.java
@@ -7,7 +7,6 @@ import com.peerius.SmartMerchandising;
 import com.peerius.utils.Context;
 import com.peerius.utils.Navigation;
 
-import cucumber.api.Scenario;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -51,8 +50,8 @@ public class StepDefinitions extends SmartMerchandising {
 	}
 	
 	@Then("^I Create invalid Campaign with name \"(.*?)\"$")
-	public void i_Create_invalid_Campaign_with_name(String campaign, Scenario scenario) throws Throwable {
-		createCampaignSimple(campaign, "Product Page", "product",
+	public void i_Create_invalid_Campaign_with_name(String campaign) throws Throwable {
+		createCampaignSimple(campaign, "Product Page", "producthorizontal",
 				"(r.productset<>\"AJ10043\")");
 	    
 	}
