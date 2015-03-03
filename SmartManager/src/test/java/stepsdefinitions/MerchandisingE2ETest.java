b5766f49693177c3ec0f2925e254bb5bb3776caf
Zach Sulemain <zach.suleiman@peerius.com>
2015-03-03 13:37:25 +0000

    "Profile Setup"

--- SmartManager/src/test/java/stepsdefinitions/MerchandisingE2ETest.java
+++ SmartManager/src/test/java/stepsdefinitions/MerchandisingE2ETest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions( features="./src", glue={"com.stepdefinitions", "stepsdefinitions"}, plugin={"pretty", "html:target/cucumber-html-report", "rerun:target/rerun.txt"})
-public class TestRunner {
+public class MerchandisingE2ETest {
 
 }
 				
