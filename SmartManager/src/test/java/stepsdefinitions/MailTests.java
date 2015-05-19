27d0817ddfc6f22a9f665f5d8c0341c4635bb8d0
Zach Sulemain <zach.suleiman@peerius.com>
2015-05-19 13:34:51 +0100

    "Edited POM to Add Profiles"

--- SmartManager/src/test/java/stepsdefinitions/MailTests.java
+++ SmartManager/src/test/java/stepsdefinitions/MailTests.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features="./src/test/resources/MailFeatures/", glue={"com.stepdefinitions", "stepsdefinitions"}, tags={"@CreateMailCampaign"}, plugin={"pretty", "html:target/cucumber-html-report","json:target/cucumber.json", "rerun:target/rerun.txt"})
+@CucumberOptions( features="./src/test/resources/MailFeatures/", glue={"com.stepdefinitions", "stepsdefinitions"}, plugin={"pretty", "html:target/cucumber-html-report","json:target/cucumber.json", "rerun:target/rerun.txt"})
 public class MailTests {
 
 }
