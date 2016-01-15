6a43ef5733f5103a7326a24bc0c7cb9df07f4252
unknown <ajoshi@EMBIOPTERA.peerius.local>
2016-01-15 14:59:39 +0000

    Added test runner for content

---
+++ SmartManager/src/test/java/stepsdefinitions/ContentTests.java
@@ -0,0 +1,13 @@
+package stepsdefinitions;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions( features="./src/test/resources/ContentFeatures/", glue={"com.stepdefinitions", "stepsdefinitions"}, plugin={"pretty", "html:target/cucumber-html-report","json:target/cucumber.json", "rerun:target/rerun.txt"})
+public class ContentTests {
+
+}
