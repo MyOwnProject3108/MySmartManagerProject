e35f870e84a4fc5d939759cc138524590161ae80
Zach Sulemain <zach.suleiman@peerius.com>
2016-02-26 17:09:56 +0000

    "Mail Test Runner"

--- SmartManager/src/test/java/stepsdefinitions/MailTests.java
+++ SmartManager/src/test/java/stepsdefinitions/MailTests.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions( features="./src/test/resources/",glue={"com.stepdefinitions", "stepsdefinitions"},
+@CucumberOptions( features="./src/test/resources/MailFeatures/",glue={"com.stepdefinitions", "stepsdefinitions"},
 plugin={"pretty", "html:target/cucumber-html-report","json:target/cucumber.json", "rerun:target/rerun.txt"})
 public class MailTests {
 
